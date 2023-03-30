acf6c6d3468f5295f03bf968687c6414ae0dc795
paigeman <53284808+paigeman@users.noreply.github.com>
2023-03-30 13:04:42 +0800

    finish q1637

---
+++ src/main/java/org/fade/leetcode/editor/cn/WidestVerticalAreaBetweenTwoPointsContainingNoPointsMain.java
@@ -0,0 +1,68 @@
+/**
+给你 n 个二维平面上的点 points ，其中 points[i] = [xi, yi] ，请你返回两点之间内部不包含任何点的 最宽垂直区域 的宽度。 
+
+ 垂直区域 的定义是固定宽度，而 y 轴上无限延伸的一块区域（也就是高度为无穷大）。 最宽垂直区域 为宽度最大的一个垂直区域。 
+
+ 请注意，垂直区域 边上 的点 不在 区域内。 
+
+ 
+
+ 示例 1： 
+ 
+ 
+输入：points = [[8,7],[9,9],[7,4],[9,7]]
+输出：1
+解释：红色区域和蓝色区域都是最优区域。
+ 
+
+ 示例 2： 
+
+ 
+输入：points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
+输出：3
+ 
+
+ 
+
+ 提示： 
+
+ 
+ n == points.length 
+ 2 <= n <= 10⁵ 
+ points[i].length == 2 
+ 0 <= xi, yi <= 10⁹ 
+ 
+
+ Related Topics 数组 排序 👍 45 👎 0
+
+*/
+  
+package org.fade.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class WidestVerticalAreaBetweenTwoPointsContainingNoPointsMain {
+      
+    public static void main(String[] args) {
+        Solution solution = new WidestVerticalAreaBetweenTwoPointsContainingNoPointsMain().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int maxWidthOfVerticalArea(int[][] points) {
+            Arrays.sort(points, Comparator.comparingInt(x -> x[0]));
+            int ans = Integer.MIN_VALUE;
+            for (int i = 1; i < points.length; ++i) {
+                if (points[i][0] - points[i - 1][0] > ans) {
+                    ans = points[i][0] - points[i - 1][0];
+                }
+            }
+            return ans;
+        }
+
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+        
+}
